f3d4eccf5c35888d4e2d761781f7e8f0144c8a9a
zauberhaus <dil@zauberhaus.de>
2026-07-09 13:35:28 +1200

    fix: CVE-2026-39822

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -3,7 +3,8 @@
 set -eo pipefail
 
 PRG=`git remote get-url origin | xargs basename -s .git`
-PLATFORM="${PLATFORM:-linux/arm64 linux/amd64 darwin/amd64 darwin/arm64 linux/riscv64 linux/mips64le linux/ppc64le}"
+PLATFORM="${PLATFORM:-linux/arm64 linux/amd64 darwin/amd64 darwin/arm64 linux/riscv64 linux/ppc64le}"
+GO_IMAGE="${GO_IMAGE:-golang:1.26.5}"
 
 DIFF=`git diff --stat`
 NOW="`date +%Y-%m-%dT%T%z`"
@@ -11,29 +12,47 @@ COMMIT=`git rev-parse HEAD`
 
 mkdir -p ./dist
 
+CACHE_DIR="${CACHE_DIR:-$HOME/.cache/$PRG-build-dist}"
+mkdir -p "$CACHE_DIR/gomodcache" "$CACHE_DIR/gocache"
+
 if [ -z "$VERSION" ] ; then
     VERSION=$(git describe --tags --always --dirty)
-fi 
+fi
 
 FLAGS="-X 'main.gitCommit=$COMMIT' -X 'main.buildTime=$NOW' -X 'main.treeState=$STATE' -X 'main.tag=$VERSION' -w -s -extldflags \"-static\""
-export CGO_ENABLED=0
+
+BUILD_SCRIPT="git config --global --add safe.directory /src"$'\n'
 
 for P in $PLATFORM ; do
     IFS='/' read -r -a parts <<< "$P"
 
-    export GOOS=${parts[0]}
-    export arch=${parts[1]}
+    GOOS=${parts[0]}
+    arch=${parts[1]}
 
     if expr "$arch" : "armv6$" 1>/dev/null; then
-        export GOARCH=arm 
-        export GOARM=6
+        GOARCH=arm
+        GOARM=6
     elif expr "$arch" : "armv7$" 1>/dev/null; then
-        export GOARCH=arm
-        export GOARM=7
-    else 
-        export GOARCH=$arch
+        GOARCH=arm
+        GOARM=7
+    else
+        GOARCH=$arch
+        GOARM=
     fi
 
-    echo "Build $PRG-$GOOS-$arch $VERSION" 
-    go build -tags prod -o ./dist/$PRG-$GOOS-$arch -ldflags "$FLAGS"
+    BUILD_SCRIPT+="echo 'Build $PRG-$GOOS-$arch $VERSION'"$'\n'
+    BUILD_SCRIPT+="GOOS=$GOOS GOARCH=$GOARCH GOARM=$GOARM go build -tags prod -o ./dist/$PRG-$GOOS-$arch -ldflags \"$FLAGS\""$'\n'
 done
+
+docker run --rm \
+    --user "$(id -u):$(id -g)" \
+    -e CGO_ENABLED=0 \
+    -e HOME=/tmp \
+    -e GOCACHE=/tmp/gocache \
+    -e GOMODCACHE=/tmp/gomodcache \
+    -v "$PWD":/src \
+    -v "$CACHE_DIR/gomodcache":/tmp/gomodcache \
+    -v "$CACHE_DIR/gocache":/tmp/gocache \
+    -w /src \
+    "$GO_IMAGE" \
+    sh -c "$BUILD_SCRIPT"
